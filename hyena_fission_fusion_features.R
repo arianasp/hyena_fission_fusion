255810cf1f1711262c84003e6856488617bc799f
Eli Strauss <straussed@gmail.com>
2021-01-13 09:20:10 +0100

    break together period into sections by optimizing a three-piece segmented regression

--- hyena_fission_fusion_features.R
+++ hyena_fission_fusion_features.R
@@ -56,19 +56,25 @@ for(i in 1:nrow(together.seqs)){
   
   ## Identify time points
   prev.dists <- dyad.dists[ind.i, ind.j,1:t0]
-  #Start
-  far.times <- which(prev.dists > R.fission)
   
-  ## Exclude events that are too early to have prior data where they are apart
-  if(!(length(far.times) > 0))
-    next
-  together.seqs$t.start[i] <- max(far.times, na.rm = TRUE)
-  t.start <- together.seqs$t.start[i]
-  together.seqs$start.exact[i] <- !is.na(dyad.dists[ind.i, ind.j, t.start+1])
   #End
   together.seqs$t.end[i] <- tf
   t.end <- tf
   together.seqs$end.exact[i] <- together.seqs[i,'tf.exact']
+  
+  #Start
+  far.times <- which(prev.dists > R.fission)
+  
+  ## Exclude events that are too early to have prior data where they are apart
+  if(!(length(far.times) > 0)){
+    together.seqs$start.exact[i] <- FALSE
+    next
+  }
+    
+  together.seqs$t.start[i] <- max(far.times, na.rm = TRUE)
+  t.start <- together.seqs$t.start[i]
+  together.seqs$start.exact[i] <- !is.na(dyad.dists[ind.i, ind.j, t.start+1])
+  
   #Before - different for each individual
   before.steps <- seq(t.start, 1, -1)
   together.seqs$t.before.i[i] <- t.start - first.passage.time(x = xs[ind.i,before.steps],
@@ -122,15 +128,108 @@ y.closest.j <- ys[cbind(together.seqs$j, together.seqs$t.closest)]
 
 
 ### Displacement
-together.seqs$disp.start.end.i <- sqrt((x.start.i-x.end.i)^2 + (y.start.i-y.end.i)^2)
-hist(log(together.seqs$disp.start.end.i))
-hist(together.seqs$disp.start.end.i, xlim = c(0,1000), breaks = 1000)
+together.seqs$disp.during.i <- sqrt((x.start.i-x.end.i)^2 + (y.start.i-y.end.i)^2)
+together.seqs$disp.before.i <- sqrt((x.before.i-x.start.i)^2 + (y.before.i-y.start.i)^2)
+together.seqs$disp.after.i <- sqrt((x.end.i-x.after.i)^2 + (y.end.i-y.after.i)^2)
+
+together.seqs$disp.during.j <- sqrt((x.start.j-x.end.j)^2 + (y.start.j-y.end.j)^2)
+together.seqs$disp.before.j <- sqrt((x.before.j-x.start.j)^2 + (y.before.j-y.start.j)^2)
+together.seqs$disp.after.j <- sqrt((x.end.j-x.after.j)^2 + (y.end.j-y.after.j)^2)
+
+### Time
+together.seqs$duration.before.i <- together.seqs$t.start - together.seqs$t.before.i
+together.seqs$duration.after.i <- together.seqs$t.after.i - together.seqs$t.end
+
+together.seqs$duration.during <- together.seqs$t.end - together.seqs$t.start
+
+together.seqs$duration.before.j <- together.seqs$t.start - together.seqs$t.before.j
+together.seqs$duration.after.j <- together.seqs$t.after.j - together.seqs$t.end
+
+### Speed
+together.seqs.speed.before.i <- together.seqs$disp.before.i / together.seqs$duration.before.i
+together.seqs$speed.during.i <- together.seqs$disp.during.i / together.seqs$duration.during
+together.seqs$speed.after.i <- together.seqs$disp.after.i / together.seqs$duration.after.i
+
+together.seqs.speed.before.j <- together.seqs$disp.before.j / together.seqs$duration.before.j
+together.seqs$speed.during.j <- together.seqs$disp.during.j / together.seqs$duration.during
+together.seqs$speed.after.j <- together.seqs$disp.after.j / together.seqs$duration.after.j
+
+
+
+
+
+
+r = 1
+
+fp <- list(x0 = x[1], 
+           y0 = y[1],
+           xf = x[length(x)],
+           yf = y[length(y)])
+
+p <- constrOptim(theta = c(fp$x0+5, fp$xf-5, 1), f = ls_error, fixed.parameters = fp, x = x, y =y,
+                 ui = matrix(nrow  = 7, ncol = 3, byrow = TRUE,
+                             data = c(1,0,0,
+                                      -1,0,0,
+                                      0,1,0,
+                                      0,-1,0,
+                                      0,0,1,
+                                      0,0,-1,
+                                      -1,1,0)),
+                 ci = c(fp$x0+1, -fp$xf-1, fp$x0+1, -fp$xf-1, 0, -fp$yf , 0),
+                 grad = NULL)
+
+# p <- optim(par = c(fp$x0+5, fp$xf-5, 0), fn = ls_error, fixed.parameters = fp, x = x, y = y, method = "L-BFGS-B",
+#            lower = c(fp$x0+1, fp$x0+1, 0), upper = c(fp$xf-1, fp$xf-1, fp$yf))
+
+
+plot_dyad_dists(r)
+lines(x =x, y = fission_fusion_function(x, p$par[1], p$par[2], p$par[3], fp), col = 'red')
+
+
+together.seqs.exact <- together.seqs[together.seqs$start.exact & together.seqs$end.exact,]
+together.seqs.exact[,c('b1', 'b2', 'y.intercept')] <- NA
+for(r in 1:nrow(together.seqs.exact)){
+  
+  ### Remove one case at the very start of the data
+  if(is.na(together.seqs.exact$t.start[r]))
+    next
+  
+  y = dyad.dists[together.seqs.exact$i[r], together.seqs.exact$j[r], together.seqs.exact$t.start[r]:together.seqs.exact$t.end[r]]
+  x = together.seqs.exact$t.start[r]:together.seqs.exact$t.end[r]
+  fp <- list(x0 = x[1], 
+             y0 = y[1],
+             xf = x[length(x)],
+             yf = y[length(y)])
+  
+  
+  p <- constrOptim(theta = c(fp$x0+5, fp$xf-5, 1), f = ls_error, fixed.parameters = fp, x = x, y =y,
+                   ui = matrix(nrow  = 7, ncol = 3, byrow = TRUE,
+                               data = c(1,0,0,
+                                        -1,0,0,
+                                        0,1,0,
+                                        0,-1,0,
+                                        0,0,1,
+                                        0,0,-1,
+                                        -1,1,0)),
+                   ci = c(fp$x0+1, -fp$xf-1, fp$x0+1, -fp$xf-1, 0, -fp$yf , 0),
+                   grad = NULL)
+  
+  
+  together.seqs.exact[r,c('b1', 'b2', 'y.intercept')] <-p$par
+}
+
+
+
+
+
+
+
 
 
 
 
 #### Random plots for discussion 2021-01-11
-hist(together.seqs$t.end - together.seqs$t.start, breaks = 1000,
+hist(together.seqs$duration, breaks = 1000,
      main = 'Distribution of duration of fusion events', xlab = 'Duration')
 
 hist(together.seqs$disp.start.end.i, breaks = 1000,
@@ -138,19 +237,61 @@ hist(together.seqs$disp.start.end.i, breaks = 1000,
 hist(together.seqs$disp.start.end.i, xlim = c(0,1000), breaks = 1000,
      main = 'Distribution of distances travelled (zoomed in)', xlab = 'Distance travelled')
 
-disp.200 <- which(together.seqs$disp.start.end.i >= 195 & together.seqs$disp.start.end.i <= 205)
+hist(together.seqs.exact$y.intercept, breaks = 100)
+hist(together.seqs.exact$b2 - together.seqs.exact$b1, breaks = 100)
 
-for(r in disp.200){
-  x.i <- xs[together.seqs$i[r], together.seqs$t.start[r]:together.seqs$t.end[r]]
-  y.i <- ys[together.seqs$i[r], together.seqs$t.start[r]:together.seqs$t.end[r]]
-  x.j <- xs[together.seqs$j[r], together.seqs$t.start[r]:together.seqs$t.end[r]]
-  y.j <- ys[together.seqs$j[r], together.seqs$t.start[r]:together.seqs$t.end[r]]
-  
-  
-  plot(x.i, y.i,asp=1, ylim = c(min(c(y.i, y.j), na.rm =TRUE), max(c(y.i, y.j), na.rm = TRUE)),
-       xlim = c(min(c(x.i, x.j), na.rm =TRUE), max(c(x.i, x.j), na.rm = TRUE)), col = 'blue')
-  points(x.j, y.j, col = 'red')
-}
+
+disp.200 <- which(together.seqs$disp.during.i >= 195 & together.seqs$disp.during.i <= 205)
+plot_events(disp.200)
+
+disp.400 <- which(together.seqs$disp.during.i >= 395, together.seqs$disp.during.i <= 405)
+plot_events(disp.400)
+
+random.50 <- sample(1:nrow(together.seqs), 50)
+plot_events(random.20)
+
+random.10.exact <- sample(1:nrow(together.seqs.exact), 10)
+plot_events(10)
+plot_canonical_shape(10)
+
+disp.over.500 <- which(together.seqs$disp.during.i >= 500)
+plot_events(disp.over.500)
+
+
+
+r=200
+plot_events(r)
+
+plot_canonical_shape(rows = c(200, 100), dyad.dists = dyad.dists, together.seqs.exact = together.seqs.exact)
+
+
+
+y = dyad.dists[together.seqs.exact$i[r], together.seqs.exact$j[r], together.seqs.exact$t.start[r]:together.seqs.exact$t.end[r]]
+x = together.seqs.exact$t.start[r]:together.seqs.exact$t.end[r]
+plot(y = y, type = 'l',
+     x = x,
+     xlab = 'Time (s)', ylab = 'Distance between individuals (m)')
+
+
+
+fp <- list(x0 = x[1], 
+           y0 = y[1],
+           xf = x[length(x)],
+           yf = y[length(y)]) 
+
+p <- constrOptim(theta = c(fp$x0+5, fp$xf-5, 1), f = ls_error, fixed.parameters = fp, x = x, y =y,
+                 ui = matrix(nrow  = 7, ncol = 3, byrow = TRUE,
+                             data = c(1,0,0,
+                                      -1,0,0,
+                                      0,1,0,
+                                      0,-1,0,
+                                      0,0,1,
+                                      0,0,-1,
+                                      -1,1,0)),
+                 ci = c(fp$x0+1, -fp$xf-1, fp$x0+1, -fp$xf-1, 0, -fp$yf , 0),
+                 grad = NULL)
+
+lines(x =x, y = fission_fusion_function(x, p$par[1], p$par[2], p$par[3], fp), col = 'red')
 
 
 
--- hyena_functions.R
+++ hyena_functions.R
@@ -156,3 +156,101 @@ first.passage.time <- function(x, y, R){
   }
   
 }
+
+### Find parameters defining the 'canonical shape' that best matches data for a fission-fusion event
+fission_fusion_function <- function(x, b1, b2, b.y.intercept, fixed.parameters){
+  x0 <- fp$x0
+  y0 <- fp$y0
+  xf <- fp$xf
+  yf <- fp$yf
+  
+  val <- rep(NA, length(x))
+  ## Aproach
+  m1 <- (b.y.intercept - y0)/(b1 - x0)
+  val[which(x < b1)] <- y0 + m1*(x[which(x < b1)]-x0)
+  
+  ## Together
+  val[which(x < b2 &  x >= b1)] <- b.y.intercept
+  
+  ## Depart
+  m2 <- (yf-b.y.intercept)/(xf - b2)
+  val[which(x >= b2)] <- b.y.intercept + m2*(x[which(x >= b2)] - b2)
+  return(val)
+}
+
+
+### Calculate least squared error for fission_fusion_function. For optimization. 
+ls_error <- function(params, fixed.parameters, x, y){
+  y.fit <- fission_fusion_function(x = x, b1 = params[1], b2 = params[2],
+                                   b.y.intercept = params[3], fixed.parameters = fixed.parameters)
+  
+  error <- sum((y.fit - y)^2, na.rm = TRUE)
+  return(error)
+}
+
+
+plot_dyad_dists <- function(indices){
+  for(r in indices){
+    plot(y = dyad.dists[together.seqs$i[r], together.seqs$j[r], together.seqs$t.start[r]:together.seqs$t.end[r]], type = 'l',
+         x = together.seqs$t.start[r]:together.seqs$t.end[r],
+         xlab = 'Time (s)', ylab = 'Distance between individuals (m)')
+  }
+}
+
+plot_canonical_shape <- function(rows, dyad.dists, together.seqs.exact){
+  for(r in rows){
+    
+    y <- dyad.dists[together.seqs.exact$i[r], together.seqs.exact$j[r], together.seqs.exact$t.start[r]:together.seqs.exact$t.end[r]]
+    x <- together.seqs.exact$t.start[r]:together.seqs.exact$t.end[r]
+    plot(y = y, type = 'l',
+         x = x,
+         xlab = 'Time (s)', ylab = 'Distance between individuals (m)')
+    
+   
+  
+    fp <- list(x0 = x[1], 
+               y0 = y[1],
+               xf = x[length(x)],
+               yf = y[length(y)]) 
+    
+    # lines(x =x, y = fission_fusion_function(x, p$par[1], p$par[2], p$par[3], fp), col = 'red')
+    
+    l <- lines(x =x, y = fission_fusion_function(x = x, b1 = together.seqs.exact$b1[r], b2 = together.seqs.exact$b2[r],
+                                            b.y.intercept = together.seqs.exact$y.intercept[r], fixed.parameters = fp), col = 'red')
+
+    print(dd.plot, l)
+    
+  }
+}
+
+
+plot_events <- function(indices){
+  for(r in indices){
+    x.i <- xs[together.seqs$i[r], together.seqs$t.start[r]:together.seqs$t.end[r]]
+    x.i <- x.i - mean(x.i, na.rm = TRUE)
+    y.i <- ys[together.seqs$i[r], together.seqs$t.start[r]:together.seqs$t.end[r]]
+    y.i <- y.i - mean(y.i, na.rm = TRUE)
+    x.j <- xs[together.seqs$j[r], together.seqs$t.start[r]:together.seqs$t.end[r]]
+    x.j <- x.j - mean(x.j, na.rm = TRUE)
+    y.j <- ys[together.seqs$j[r], together.seqs$t.start[r]:together.seqs$t.end[r]]
+    y.j <- y.j - mean(y.j, na.rm = TRUE)
+    
+    
+    plot(x.i, y.i,asp=1, ylim = c(min(c(y.i, y.j), na.rm =TRUE), max(c(y.i, y.j), na.rm = TRUE)),
+         xlim = c(min(c(x.i, x.j), na.rm =TRUE), max(c(x.i, x.j), na.rm = TRUE)), col = scales::alpha('blue', 0.5),
+         cex = seq(0.1, 1, length.out = length(x.i)), main = r)
+    points(x.j, y.j, col = scales::alpha('red', 0.5), cex = seq(0.1, 1, length.out = length(x.i)))
+    x.lims <- par()$usr[1:2]
+    y.lims <- par()$usr[3:4]
+    text(paste0('displacement.i = ', round(together.seqs$disp.during.i[r],2), '\n',
+                'duration.i (mins) = ', round(together.seqs$duration.during[r]/60, 2), '\n',
+                'speed.i (m/min) = ', round(together.seqs$speed.during.i[r] * 60, 2), '\n'), 
+         x = x.lims[1], y = y.lims[2] - 0.2*(y.lims[2] - y.lims[1]), col = 'blue', pos = 4)
+    text(paste0('displacement.j = ', round(together.seqs$disp.during.j[r],2), '\n',
+                'duration.j (mins) = ', round(together.seqs$duration.during[r]/60, 2), '\n',
+                'speed.j (m/min) = ', round(together.seqs$speed.during.j[r]*60, 2), '\n'),
+         x = x.lims[2], y = y.lims[2] - 0.2*(y.lims[2] - y.lims[1]), col = 'red', pos = 2)
+  }
+}
+
+
