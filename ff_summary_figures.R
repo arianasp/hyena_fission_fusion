1f983828434495115359534b8c05c9a0f5edaf94
Eli Strauss <straussed@gmail.com>
2021-09-22 09:27:49 +0200

    adjust plotting symbol and width figure 3

--- ff_summary_figures.R
+++ ff_summary_figures.R
@@ -413,11 +413,11 @@ plotdat.denblock$lab.den <- 'Den'
 plotdat.denblock$lab.nonden <- 'Non-Den'
 p_nevents <- ggplot(data = plotdat.denblock) + 
   geom_violin(mapping = aes(x = lab.all, y = nevents), fill = 'gray30', color = 'gray30') + 
-  geom_point(x = 1, y = events.tot.data, shape = '|', size = 7)+
+  geom_point(x = 1, y = events.tot.data, shape = '|', size = 6)+
   geom_violin(mapping = aes(x = lab.den, y = denevents), fill = colors[2], color = colors[2]) + 
-  geom_point(x = 2, y = events.tot.den.data, shape = '|', size = 7)+
+  geom_point(x = 2, y = events.tot.den.data, shape = '|', size = 6)+
   geom_violin(mapping = aes(x = lab.nonden, y = nondenevents), fill = colors[1], color = colors[1]) + 
-  geom_point(x = 3, y = events.tot.nonden.data, shape = '|', size = 7)+
+  geom_point(x = 3, y = events.tot.nonden.data, shape = '|', size = 6)+
   labs(title="",x="", y = "Number of fission-fusion events") +
   ylim(0, events.tot.data) + 
   theme_classic(base_size = 12) + 
